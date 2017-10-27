68f31615996a17e877c4372c77fbe4a4e735029e
wrCisco <lbjma@tiscali.it>
2017-10-27 17:26:48 +0200

    Add tests for add_default_prefix and ignore_selectors

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -7,11 +7,13 @@ import os
 import cssutils
 
 import plugin as p
+import customcssutils
 
 
 class TestPlugin(unittest.TestCase):
 
     def setUp(self):
+        cssutils.setSerializer(customcssutils.MyCSSSerializer())
         self.css = cssutils.parseFile(os.path.join(os.path.dirname(__file__),
                                                    'docs', 'base.css'))
 
@@ -26,8 +28,33 @@ class TestPlugin(unittest.TestCase):
                            'a': 'https://www.w3.org/1999/xhtml'}, 'a'))
 
     def test_add_default_prefix(self):
-        self.css.namespaces['aa'] = 'https://www.w3.org/1999/xhtml'
-        self.assertEqual(p.add_default_prefix('aa', 'p.ex1 + p.ex2'),
-                         'aa|p.ex1 + aa|p.ex2')
-        self.assertEqual(p.add_default_prefix('aa', '.ex1:nth-child(2n+1) > svg|h2  :not(p) .\:not'),
-                         '.ex1:nth-child(2n+1) > svg|h2  :not(aa|p) .\:not')
+        # Some selectors directly given to the tested function
+        self.assertEqual(p.add_default_prefix('aa', 'p.ex1 > strong.ex2'),
+                         'aa|p.ex1 > aa|strong.ex2')
+        self.assertEqual(p.add_default_prefix('aa', '.ex1:nth-child(2n+1) > '
+                                                    'svg|text.\ startingSpace \t:not(p) '
+                                                    '.\:notANotSelector\(.\\201C quoted\\201D'),
+                         '.ex1:nth-child(2n+1) > '
+                         'svg|text.\ startingSpace \t:not(aa|p) '
+                         '.\:notANotSelector\(.\\201C quoted\\201D')
+        # And some mangled by cssutils
+        self.css.namespaces['svg'] = 'https://www.w3.org/2000/svg'
+        self.assertTrue('svg' in self.css.namespaces)
+        self.css.add(cssutils.css.CSSStyleRule(selectorText='p+p.\\201C quoted\\201D \nspan',
+                                               style=cssutils.css.CSSStyleDeclaration('')))
+        self.assertEqual(p.add_default_prefix('aa', self.css.cssRules[-1].selectorText),
+                         'aa|p + aa|p.“quoted” aa|span')
+        self.css.add('.ex1:nth-child(2n+1) > svg|text.\ startingSpace \t:not(p) '
+                     '.\:notANotSelector\(.\\201C quoted\\201D {}')
+        self.assertEqual(p.add_default_prefix('aa', self.css.cssRules[-1].selectorText),
+                         '.ex1:nth-child(2n+1) > svg|text.\ startingSpace :not(aa|p) '
+                         '.\:notANotSelector\(.“quoted”')
+
+    def test_ignore_selectors(self):
+        for pseudo_class in p.NEVER_MATCH:
+            self.css.insertRule('{} {{}}'.format(pseudo_class))
+        for i in range(0, len(self.css.cssRules) - len(p.NEVER_MATCH)):
+            self.css.deleteRule(0)
+        for rule in self.css:
+            with self.subTest(rule=rule):
+                self.assertTrue(p.ignore_selectors(rule.selectorText))
