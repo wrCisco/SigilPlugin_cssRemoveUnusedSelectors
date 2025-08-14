971eda647344e87957f2eb2baab2dedc18001adf
Francesco Martini <wrCisco@users.noreply.github.com>
2025-08-14 23:04:32 +0200

    Update copyright notices

--- plugin.py
+++ plugin.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-# Copyright (c) 2016, 2019, 2020 Francesco Martini
+# Copyright (c) 2016, 2019, 2020, 2024, 2025 Francesco Martini
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
